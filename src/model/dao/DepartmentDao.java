3d82b270938e1841cadb4f5cf28216c5616b4955
DeLanghi <daniel@langhi.com.br>
2019-02-25 15:47:39 +0100

    SellerDaoJDBC and DaoFactory

--- src/model/dao/DepartmentDao.java
+++ src/model/dao/DepartmentDao.java
@@ -1,5 +1,14 @@
 package model.dao;
 
+import java.util.List;
+import model.entities.Department;
+
 public interface DepartmentDao {
+	
+	void insert(Department obj);					// operação responsável por inserir o banco de dados
+	void update(Department obj);
+	void deleteById(Integer id);
+	Department findByid(Integer id);				// operação é responsável por pegar o ID e procurá-lo no banco de dados; se existir irá imprimi-lo. caso contrário, retorna nulo
+	List<Department> findAll();						// aqui retornará todos os departamentos; em formato de lista
 
 }
