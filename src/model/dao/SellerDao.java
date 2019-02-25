ef9db42ec8617fccc6f26c1e688933d03aac29fc
DeLanghi <daniel@langhi.com.br>
2019-02-25 15:27:05 +0100

    DepartmentDao and SellerDao interfaces

---
+++ src/model/dao/SellerDao.java
@@ -0,0 +1,14 @@
+package model.dao;
+
+import java.util.List;
+import model.entities.Seller;
+
+public interface SellerDao {
+	
+	void insert(Seller obj);					// operação responsável por inserir o banco de dados
+	void update(Seller obj);
+	void deleteById(Integer id);
+	Seller findByid(Integer id);				// operação é responsável por pegar o ID e procurá-lo no banco de dados; se existir irá imprimi-lo. caso contrário, retorna nulo
+	List<Seller> findAll();						// aqui retornará todos os departamentos; em formato de lista
+
+}
