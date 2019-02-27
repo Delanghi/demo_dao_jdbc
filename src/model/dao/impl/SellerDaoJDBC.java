5d39fce42f09b9869c9815d349521c345fcd1a28
DeLanghi <daniel@langhi.com.br>
2019-02-27 15:37:48 +0100

    insert implementation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -28,6 +29,39 @@ public class SellerDaoJDBC implements SellerDao {
 	 // IMPLEMENTAÇÃO DOS MÉTODOS
 		@Override
 		public void insert(Seller obj) {
+			
+			PreparedStatement st = null;
+			try {				
+				st = conn.prepareStatement(
+					"INSERT INTO seller "
+					+ "(Name, Email, BirthDate, BaseSalary, DepartmentId) " 
+					+ "VALUES "
+					+ "(?, ?, ?, ?, ?)" ,
+					Statement.RETURN_GENERATED_KEYS);							// p/ acrescentar novo vendedor
+				
+				st.setString(1, obj.getName());
+				st.setString(2, obj.getEmail());
+				st.setDate(3, new java.sql.Date(obj.getBirthDate().getTime()));		//INSTANCIAR uma data do SQL
+				st.setDouble(4, obj.getBaseSalary());
+				st.setInt(5, obj.getDepartment().getId());						// 1º acessa o depto; depois pega o ID
+				
+				int rowsAffected = st.executeUpdate();
+					if(rowsAffected > 0) {
+						ResultSet rs = st.getGeneratedKeys();
+							if(rs.next()) {										// "If" pq vamos inserir apenas um dado
+								int id = rs.getInt(1);							// vai inserir na 1ª coluna das chaves
+								obj.setId(id);									
+							}
+							DB.closeResultset(rs); 								// vamos fechá-lo por aqui
+					} else {
+						throw new DbException("Unexpected error! No rows affected. ");
+					}
+			} catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+			finally {
+				DB.closeStatement(st);
+			}
 		}
 
 		@Override
